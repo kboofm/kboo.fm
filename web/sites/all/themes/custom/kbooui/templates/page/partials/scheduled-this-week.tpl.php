9cfd4e9e6d7bfed4061f465aec3dee4e144a87b1
Marcus Amargi <marcus@c4tech.com>
2015-10-15 13:23:44 -0700

    [KBOO-115] grouped week by day

--- web/sites/all/themes/custom/kbooui/templates/page/partials/scheduled-this-week.tpl.php
+++ web/sites/all/themes/custom/kbooui/templates/page/partials/scheduled-this-week.tpl.php
@@ -1,6 +1,11 @@
 <?php
-$schedule = TemplateQuery::scheduledThisWeek();
+$schedule_group = TemplateQuery::scheduledThisWeek();
 ?>
 
 <h4>This Week:</h4>
-<?php include 'schedule-list.tpl.php'; ?>
+<?php foreach ($schedule_group as $dayOfWeek => $schedule): ?>
+  <?php if ($schedule): ?>
+    <h5><?php print $dayOfWeek; ?></h5>
+    <?php include 'schedule-list.tpl.php'; ?>
+  <?php endif; ?>
+<?php endforeach; ?>
