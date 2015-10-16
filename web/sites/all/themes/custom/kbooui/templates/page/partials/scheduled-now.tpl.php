2f7b7c619f519178632baed805dc4cfefff853c4
Marcus Amargi <marcus@c4tech.com>
2015-10-16 09:04:53 -0700

    [KBOO-115] updated display for nothing on now

--- web/sites/all/themes/custom/kbooui/templates/page/partials/scheduled-now.tpl.php
+++ web/sites/all/themes/custom/kbooui/templates/page/partials/scheduled-now.tpl.php
@@ -3,4 +3,8 @@ $schedule_item = TemplateQuery::scheduledNow();
 $schedule = [$schedule_item];
 ?>
 <h4>On now:</h4>
-<?php include 'schedule-list.tpl.php'; ?>
+<?php if ($schedule[0]): ?>
+  <?php include 'schedule-list.tpl.php'; ?>
+<?php else: ?>
+  Nothing airing
+<?php endif; ?>
