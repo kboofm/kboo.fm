f78fa88511923e87e134c8927afa37f1ef920512
Marcus Amargi <marcus@c4tech.com>
2015-10-22 08:35:56 -0700

    [KBOO-206] refactored custom modules for multi-stream support

---
+++ web/sites/all/modules/custom/utilities/radio_station/templates/schedule.tpl.php
@@ -0,0 +1,12 @@
+<?php
+$theme_path = drupal_get_path('theme', 'wwozui');
+$partials = "{$theme_path}/templates/block/partials";
+?>
+
+<div class="row oz-schedule">
+  <div class="col-md-4 col-md-offset-4">
+    <?php include "{$partials}/schedule/scheduled-now.tpl.php"; ?>
+    <?php include "{$partials}/schedule/schedule-tabs.tpl.php"; ?>
+  </div>
+</div>
+
