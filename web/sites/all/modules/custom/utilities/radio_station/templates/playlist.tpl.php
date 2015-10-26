28d5d9a025ea775bd1713aa197c566c0b8733b2a
Marcus Amargi <marcus@c4tech.com>
2015-10-26 16:09:23 -0700

    [KBOO-216] added spinitron javascript widget

---
+++ web/sites/all/modules/custom/utilities/radio_station/templates/playlist.tpl.php
@@ -0,0 +1,14 @@
+<?php
+//$theme_path = drupal_get_path('theme', 'kbooui');
+//$partials = "{$theme_path}/templates/block/partials";
+?>
+
+<div class="row">
+  <div class="col-md-4"
+       data-station="kboo"
+       data-num="4"
+       id="spinitron-nowplaying">
+  </div>
+</div>
+
+<script src="//spinitron.com/js/npwidget.js"></script>
