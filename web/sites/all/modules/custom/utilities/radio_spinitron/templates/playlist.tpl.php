b99bd5711e2f49047a452189915ccf8cc25cab24
Marcus Amargi <marcus@c4tech.com>
2015-10-30 12:56:23 -0700

    [KBOO-217] get playlist by episode time added, template with html table

--- web/sites/all/modules/custom/utilities/radio_spinitron/templates/playlist.tpl.php
+++ web/sites/all/modules/custom/utilities/radio_spinitron/templates/playlist.tpl.php
@@ -1,1 +1,36 @@
-<h1>Hello Dolly!</h1>
+<table class="table table-striped table-condensed">
+  <thead>
+  <tr>
+    <th>Playlist Date</th>
+    <th>Show Name</th>
+    <th>On Air Time</th>
+    <th>Off Air Time</th>
+    <th>DJ Name</th>
+  </tr>
+  </thead>
+  <tbody>
+  <?php foreach ($spinitron_playlists as $playlist): ?>
+        <tr>
+          <td>
+            <?php print $playlist['PlaylistDate']; ?>
+          </td>
+
+          <td>
+            <?php print $playlist['ShowName']; ?>
+          </td>
+
+          <td>
+            <?php print $playlist['OnairTime']; ?>
+          </td>
+
+          <td>
+            <?php print $playlist['OffairTime']; ?>
+          </td>
+
+          <td>
+            <?php print $playlist['DJName']; ?>
+          </td>
+        </tr>
+  <?php endforeach; ?>
+  </tbody>
+</table>
