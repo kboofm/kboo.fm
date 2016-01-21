364c2e7aba6b9ee8666701387e06a5de8653d313
Drew Stephan <drew@c4tech.com>
2016-01-20 18:24:17 -0600

    [KBOO-234] styling header

--- web/sites/all/themes/custom/kbooui/templates/page/page--front.tpl.php
+++ web/sites/all/themes/custom/kbooui/templates/page/page--front.tpl.php
@@ -11,6 +11,11 @@
         <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
       <?php endif; ?>
 
+      <?php
+        include dirname(__DIR__) . "/block/partials/listen-now.tpl.php";
+        include dirname(__DIR__) . "/block/partials/schedule/on-air.tpl.php";
+      ?>
+
       <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only">Toggle navigation</span>
@@ -26,15 +31,16 @@
           <?php if (!empty($primary_nav)): ?>
             <?php print render($primary_nav); ?>
           <?php endif; ?>
-          <?php if (!empty($secondary_nav)): ?>
-            <?php print render($secondary_nav); ?>
-          <?php endif; ?>
           <?php if (!empty($page['navigation'])): ?>
             <?php print render($page['navigation']); ?>
           <?php endif; ?>
         </nav>
       </div>
     <?php endif; ?>
+
+    <?php
+      include dirname(__DIR__) . "/block/partials/donate-btn.tpl.php";
+    ?>
   </div>
 </header>
 
@@ -49,12 +55,6 @@
   </header> <!-- /#page-header -->
 
   <div class="row">
-
-    <?php if (!empty($page['sidebar_first'])): ?>
-      <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_first']); ?>
-      </aside>  <!-- /#sidebar-first -->
-    <?php endif; ?>
 
     <section<?php print $content_column_class; ?>>
       <?php if (!empty($page['highlighted'])): ?>
@@ -78,16 +78,12 @@
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
 
-      <?php
-      include dirname(__DIR__) . "/block/partials/schedule/on-air.tpl.php";
-      ?>
-
       <?php print render($page['content']); ?>
     </section>
 
-    <?php if (!empty($page['sidebar_second'])): ?>
+    <?php if (!empty($page['sidebar'])): ?>
       <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_second']); ?>
+        <?php print render($page['sidebar']); ?>
       </aside>  <!-- /#sidebar-second -->
     <?php endif; ?>
 
