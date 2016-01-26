9e72fb8f63bf7e5b229ce1f493a92b9cb9a2d2c7
Marcus Amargi <marcus@c4tech.com>
2016-01-26 10:35:00 -0800

    [KBOO-246] removed two cruft routes

--- web/sites/all/modules/custom/utilities/radio_station/templates/about-contributors.tpl.php
+++
@@ -1,9 +0,0 @@
-<ul class="margin-top-lg">
-  <?php foreach ($profiles as $profile): ?>
-    <li>
-      <a href="<?php print $profile['url']; ?>">
-        <?php print $profile['title']; ?>
-      </a>
-    </li>
-  <?php endforeach; ?>
-</ul>
