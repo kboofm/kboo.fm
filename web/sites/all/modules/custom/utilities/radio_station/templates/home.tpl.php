12aad931fd7743f589cf601b34c47113a79cf638
Marcus Amargi <marcus@c4tech.com>
2016-02-03 08:25:28 -0800

    [KBOO-119] finished implementing home route

--- web/sites/all/modules/custom/utilities/radio_station/templates/home.tpl.php
+++ web/sites/all/modules/custom/utilities/radio_station/templates/home.tpl.php
@@ -1,1 +1,17 @@
-Hello Dolly!
+<div class="row margin-vertical-lg">
+  <div class="col-sm-12">
+    <?php
+    $last = count($pager['items']) - 1;
+    foreach ($pager['items'] as $index => $item):
+      $view = node_view($item, 'teaser');
+      print drupal_render($view);
+
+      if ($index != $last):
+        print "<hr />";
+      endif;
+    endforeach;
+
+    include 'partials/pager.tpl.php';
+    ?>
+  </div>
+</div>
