d51d0ede35ea887d2fc9af528c104c2d10d213b9
Marcus Amargi <marcus@c4tech.com>
2016-02-26 14:53:41 -0800

    [KBOO-315] implemented query and template

--- web/sites/all/modules/custom/utilities/radio_station/templates/sidebar-audio.tpl.php
+++ web/sites/all/modules/custom/utilities/radio_station/templates/sidebar-audio.tpl.php
@@ -1,6 +1,23 @@
-<h3>My Own Block</h3>
+<h2 class="bg-primary">
+  Latest Audio
+</h2>
 
-<?php print $foo; ?>
 
-<div>end here</div>
+<?php foreach ($items as $item): ?>
+  <p>
+    Program Title
+    <br />
+    <a href="<?php print $item['url']; ?>">
+      <?php print $item['title']; ?>
+    </a>
+  </p>
+<?php endforeach; ?>
+
+
+<p>
+  <a class="more-link bg-primary"
+     href="#">
+    More
+  </a>
+</p>
 
