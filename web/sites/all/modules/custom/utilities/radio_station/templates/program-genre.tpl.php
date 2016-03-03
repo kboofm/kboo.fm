0cd8c915efb3383799ac1798cf5611d2701dc6c1
Marcus Amargi <marcus@c4tech.com>
2016-03-03 12:15:15 -0800

    [KBOO-330] implemented group programs by genre

--- web/sites/all/modules/custom/utilities/radio_station/templates/program-genre.tpl.php
+++ web/sites/all/modules/custom/utilities/radio_station/templates/program-genre.tpl.php
@@ -13,19 +13,19 @@
 </ul>
 
 
-<div class="row margin-vertical-lg">
-  <div class="col-sm-12">
-    <?php
-    $last = count($programs) - 1;
-    foreach ($programs as $index => $item):
-      $view = node_view($item, 'teaser');
-      print drupal_render($view);
+<?php foreach ($genres as $genre => $programs): ?>
+  <h3>
+    <?php print $genre; ?>
+  </h3>
 
-      if ($index != $last):
-        print "<hr />";
-      endif;
-    endforeach;
-    ?>
-  </div>
-</div>
 
+  <ul>
+    <?php foreach ($programs as $program): ?>
+    <li>
+      <a href="<?php print $program['url']; ?>">
+        <?php print $program['title']; ?>
+      </a>
+    </li>
+    <?php endforeach; ?>
+  </ul>
+<?php endforeach; ?>
