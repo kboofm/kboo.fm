f3799b7f93c411be6937ad69abdf7b129e1ef313
Marcus Amargi <marcus@c4tech.com>
2016-03-02 20:59:19 -0800

    [KBOO-337] implemented program custom route

---
+++ web/sites/all/modules/custom/utilities/radio_station/templates/program.tpl.php
@@ -0,0 +1,16 @@
+<div class="row margin-vertical-lg">
+  <div class="col-sm-12">
+    <?php
+    $last = count($programs) - 1;
+    foreach ($programs as $index => $item):
+      $view = node_view($item, 'teaser');
+      print drupal_render($view);
+
+      if ($index != $last):
+        print "<hr />";
+      endif;
+    endforeach;
+    ?>
+  </div>
+</div>
+
