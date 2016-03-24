a70b93af39ef24bfd422083093bbfbf3311350be
Drew Stephan <drew@c4tech.com>
2016-03-24 16:57:09 -0500

    [KBOO-437] add donate button to smaller widths

--- web/sites/all/themes/custom/kbooui/templates/block/partials/donate-btn.tpl.php
+++ web/sites/all/themes/custom/kbooui/templates/block/partials/donate-btn.tpl.php
@@ -2,3 +2,8 @@
   <img src="<?php print base_path() . path_to_theme(); ?>/images/donate.png"
        alt="Donate Now" />
 </a>
+
+<a href="/donate"
+   class="btn btn-default donate-btn-mobile visible-xs visible-sm">
+  DONATE
+</a>
