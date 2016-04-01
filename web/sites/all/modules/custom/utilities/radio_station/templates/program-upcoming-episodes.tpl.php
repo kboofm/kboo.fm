9eebc330bde3cf11849fd0bc6eb7dffa843d6607
Marcus Amargi <marcus@c4tech.com>
2016-04-01 10:47:32 -0700

    [KBOO-453] implemented custom template for upcoming episodes

--- web/sites/all/modules/custom/utilities/radio_station/templates/program-upcoming-episodes.tpl.php
+++ web/sites/all/modules/custom/utilities/radio_station/templates/program-upcoming-episodes.tpl.php
@@ -1,5 +1,9 @@
 <?php
 $last = count($upcoming) - 1;
+$formats = [
+  'date' => 'D, m/d/Y',
+  'time' => 'g:ia',
+];
 
 if ($last < 0):
   return;
@@ -12,15 +16,58 @@ endif;
   </h3>
 
   <div class="margin-vertical-lg">
-    <?php
-    foreach ($upcoming as $index => $item):
-      $view = node_view($item, 'teaser');
-      print drupal_render($view);
+    <?php foreach ($upcoming as $index => $item): ?>
+      <?php if ($item["starttime"]): ?>
+        <div>
+          <a href="<?php print $item["url"]; ?>">
+            Airs at:
 
-      if ($index != $last):
-        print "<hr />";
-      endif;
-    endforeach;
-    ?>
+            <?php
+            $start_day = Helpers::toCarbonString(
+              $formats['date'],
+              $item['starttime']
+            );
+
+            $start_time = Helpers::toCarbonString(
+              $formats['time'],
+              $item['starttime']
+            );
+
+            $end_time = Helpers::toCarbonString(
+              $formats['time'],
+              $item['endtime']
+            );
+
+            print "{$start_day} at {$start_time}";
+
+            if ($item['ends_different_day']):
+              $end_day = Helpers::toCarbonString(
+                $formats['date'],
+                $item['endtime']
+              );
+
+              print " - {$end_day} at {$end_day}";
+
+            elseif ($start_time != $end_time):
+              print " - {$end_time}";
+            endif;
+          ?>
+          </a>
+        </div>
+      <?php endif; ?>
+
+
+      <div>
+        <?php print $item['summary']; ?>
+      </div>
+
+
+      <?php
+        if ($index != $last):
+          print "<hr />";
+        endif;
+      ?>
+    <?php endforeach; ?>
   </div>
 </div>
+
