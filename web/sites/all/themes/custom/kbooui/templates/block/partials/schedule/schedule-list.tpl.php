73f92ec25476ab8547fb3a9843e7930716dc43ce
Marcus Amargi <marcus@c4tech.com>
2016-04-07 13:22:58 -0700

    [KBOO-456] removed cruft templates

--- web/sites/all/themes/custom/kbooui/templates/block/partials/schedule/schedule-list.tpl.php
+++
@@ -1,6 +0,0 @@
-<ul class="list-group"
-  data-bind="schedule-items">
-  <?php foreach ($data_items as $schedule_item): ?>
-    <?php include 'schedule-list-item.tpl.php'; ?>
-  <?php endforeach; ?>
-</ul>
