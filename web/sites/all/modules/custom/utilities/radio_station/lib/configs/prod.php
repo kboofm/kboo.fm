ac4a01c307b4eca0836a30a34a1cacfa8c09b33a
Marcus Amargi <marcus@c4tech.com>
2016-04-19 14:32:31 -0700

    [KBOO-558] example the radio station config options to allow a range of weeks data to be generated

--- web/sites/all/modules/custom/utilities/radio_station/lib/configs/prod.php
+++ web/sites/all/modules/custom/utilities/radio_station/lib/configs/prod.php
@@ -4,7 +4,9 @@ return [
   'cron_generate_episodes' => TRUE,
   'cron_automatic_ripping' => TRUE,
   'cron_attach_rips' => TRUE,
-  'cron_generate_cache_schedule' => TRUE,
+  'cron_generate_cache_schedule_this_week' => TRUE,
+  'cron_generate_cache_schedule_previous_weeks' => 1,
+  'cron_generate_cache_schedule_next_weeks' => 4,
   'onsave_clear_cache_schedule' => TRUE,
   'onsave_clear_cache_sidebar' => TRUE,
 ];
