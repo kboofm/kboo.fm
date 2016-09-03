fb953a14f2677833709040188a2535937f08c4f6
gaba <gabelula@gmail.com>
2016-09-02 21:12:05 -0700

    Cleaned the RSS feed for podcasts per program.

--- web/sites/all/modules/custom/utilities/radio_station/templates/programs-podcast.tpl.php
+++ web/sites/all/modules/custom/utilities/radio_station/templates/programs-podcast.tpl.php
@@ -1,146 +1,44 @@
-<rss version="2.0"
-     xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd"
-     xmlns:atom="http://www.w3.org/2005/Atom">
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:atom="http://www.w3.org/2005/Atom">
 
-  <?php
-  $station_name = "KBOO Community Radio";
-  $station_email = "podcast@kboo.org";
-  ?>
+  <?php $station_name = "KBOO Community Radio"; $station_email = "podcast@kboo.org";?>
 
   <channel>
-    <title>
-      <?php print $program['title']; ?>
-    </title>
-
-    <atom:link href="<?php print $program['podcast_url']; ?>"
-               rel="self"
-               type="application/rss+xml"/>
-
-
-    <link>
-      <?php print $program['url']; ?>
-    </link>
-
-
-    <language>
-      en-us
-    </language>
-
-
-    <copyright>
-      &#x2117; &amp; &#xA9; <?php print date('Y') . " " . $station_name; ?>
-    </copyright>
-
-
-    <itunes:subtitle>
-      <?php print $program['subtitle']; ?>
-    </itunes:subtitle>
-
-
-    <itunes:author>
-      <?php print $station_name; ?>
-    </itunes:author>
-
-
-    <itunes:summary>
-      <?php print $program['summary']; ?>
-    </itunes:summary>
-
-
-    <description>
-      <?php print $program['description']; ?>
-    </description>
-
-
+    <title><?php print $program['title']; ?></title>
+    <atom:link href="<?php print $program['podcast_url']; ?>" rel="self" type="application/rss+xml"/>
+    <link><?php print $program['url']; ?></link>
+    <language>en-us</language>
+    <copyright>&#x2117; &amp; &#xA9; <?php print date('Y') . " " . $station_name; ?></copyright>
     <itunes:owner>
-      <itunes:name>
-        <?php print $station_name; ?>
-      </itunes:name>
-
-
-      <itunes:email>
-        <?php print $station_email; ?>
-      </itunes:email>
+      <itunes:name><?php print $station_name; ?></itunes:name>
     </itunes:owner>
-
-
-    <itunes:image href="<?php print $program['image']; ?>" />
-
-
-    <itunes:category text="">
-      <itunes:category text="" />
+    <itunes:category text="Public Radio">
+      <itunes:category text="Public Radio" />
     </itunes:category>
-
-
-    <itunes:explicit></itunes:explicit>
-
-
-    <image>
-      <url>
-        <?php print $program['image']; ?>
-      </url>
-
-
-      <title>
-        <?php print $program['title']; ?>
-      </title>
-
-
-      <link>
-        <?php print $program['url']; ?>
-      </link>
-    </image>
-
+    <itunes:explicit><?php print $program['explicit']; ?></itunes:explicit>
+    <itunes:image>
+      <url><?php print $program['image']; ?></url>
+      <title><?php print $program['title']; ?></title>
+      <link><?php print $program['url']; ?></link>
+    </itunes:image>
 
     <?php foreach ($items as $item): ?>
       <item>
-        <title>
-          <?php print $item['title']; ?>
-        </title>
+        <title><?php print $item['title']; ?></title>
 
+        <link><?php print $item['url']; ?></link>
 
-        <link>
-          <?php print $item['url']; ?>
-        </link>
+        <pubDate><?php print $item['published_date']; ?></pubDate>
 
+        <itunes:author><?php print $station_name ?></itunes:author>
 
-        <pubDate>
-          <?php print $item['published_date']; ?>
-        </pubDate>
-
-
-        <description>
-          <?php print $item['description']; ?>
-        </description>
-
-
-        <itunes:author>
-          <?php print $station_name ?>
-        </itunes:author>
-
-
-        <itunes:subtitle>
-          <?php print $item['subtitle']; ?>
-        </itunes:subtitle>
-
-
-        <itunes:summary>
-          <?php print $item['summary']; ?>
-        </itunes:summary>
-
+        <itunes:summary><?php print $item['summary']; ?></itunes:summary>
 
         <itunes:image href="<?php print $item['image']; ?>" />
 
+        <enclosure url="<?php print $item['audio_file']; ?>" length="<?php print $item['duration']; ?>" type="audio/mpeg"/>
 
-        <enclosure url="<?php print $item['audio_file']; ?>"
-                   length="<?php print $item['duration']; ?>"
-                   type="audio/mpeg"/>
-
-
-        <guid>
-          <?php print $item['url']; ?>
-        </guid>
-
+        <guid><?php print $item['url']; ?></guid>
 
         <itunes:keywords></itunes:keywords>
       </item>
