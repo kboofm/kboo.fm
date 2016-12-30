6eee74c59da2f99809d3c56328507ec59b5ca768
Darin Avery <darin.avery@gmail.com>
2016-12-29 17:17:17 -0800

    changes to put hosted by list on profile pages, using same renderer as the program content list.  todo: look into the player.

---
+++ web/sites/all/modules/custom/utilities/radio_station/templates/profile-content-list.tpl.php
@@ -0,0 +1,32 @@
+<div class="margin-vertical-lg">
+  <?php
+#see radio_station_preprocess_radio_template_profile_content_list in radio_station.module
+if($already_rendered)
+{
+	print $pager;
+}
+else
+{
+
+  $last = count($pager['items']) - 1;
+  foreach ($pager['items'] as $index => $item):
+    $view = node_view($item, 'teaser');
+    print drupal_render($view);
+
+    if ($index != $last):
+      print "<hr />";
+    endif;
+  endforeach;
+
+  if (isset($content_url)):
+    ?>
+    <h4>
+      <a href="<?php print $content_url; ?>">
+        View latest content for <?php print $program['title']; ?>
+      </a>
+    </h4>
+    <?php
+  endif;
+}
+  ?>
+</div>
