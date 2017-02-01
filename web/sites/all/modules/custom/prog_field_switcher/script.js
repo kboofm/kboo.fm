1b473dce049c052d18ee784dfa35d7037d335b95
Darin Avery <darin.avery@gmail.com>
2017-01-31 19:31:21 -0800

    added pub date to auto created nodes in StationContentComponent and added script to autoset pub date as air date is changed

--- web/sites/all/modules/custom/prog_field_switcher/script.js
+++ web/sites/all/modules/custom/prog_field_switcher/script.js
@@ -35,7 +35,48 @@
 				var hour = time.substr(0,2);
 				setExplicit(ampm, hour);
 			});
-			$('#edit-field-air-time-und-0-value-timeEntry-popup-1').change();
+			function pad(number)
+			{
+				if(number < 10)
+				{
+					return '0' + number;
+				}
+				return number;
+			}
+			function getCurDate()
+			{
+				var date = $('#edit-field-air-time-und-0-value-datepicker-popup-0').val();
+				date = date.split('/');
+				var time = $('#edit-field-air-time-und-0-value-timeEntry-popup-1').val();
+				var ampm = time.substr(-2);
+				time = time.split(':');
+				time[1] = time[1].substr(0, 2);
+				if(ampm == 'am' && time[0] == '12')
+				{
+					time[0] = '0';
+				}
+				if(ampm == 'pm' && time[0] != '12')
+				{
+					time[0] = Number(time[0]) + 12;
+				}
+				var dd = new Date(date[2], date[0]-1, date[1], time[0], time[1]);
+				return pad((Number(dd.getMonth()) + 1)) + '/' + dd.getDate() + '/' + dd.getFullYear() + ' - ' + pad(dd.getHours()) + ':' + pad(dd.getMinutes());
+			}
+			function setPubDate(datetime)
+			{
+				$('#edit-field-published-date-und-0-value-date').val(datetime);
+			}
+			function getSetPubDate()
+			{
+				var datetime = getCurDate();
+				setPubDate(datetime);
+			}
+			$('#edit-field-air-time-und-0-value-datepicker-popup-0').change(function(){
+				getSetPubDate();
+			});
+			$('#edit-field-air-time-und-0-value-timeEntry-popup-1').change(function(){
+				getSetPubDate();
+			});
 		}
 	};
 }(jQuery));
